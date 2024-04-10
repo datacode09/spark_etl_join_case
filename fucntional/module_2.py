5aca59e3ab22eef58f5e024e78808205ae1155c8
Abul Fahad <117757435+datacode09@users.noreply.github.com>
2024-04-10 11:46:29 -0400

    Update module_2.py

--- fucntional/module_2.py
+++ fucntional/module_2.py
@@ -3,6 +3,8 @@ from pyspark.sql.functions import coalesce, lit, concat, concat_ws, col, trim, u
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType, BooleanType, TimestampType
 import logging
 import os
+from pyspark.sql.functions import get_json_object
+
 
 # Initialize logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -42,7 +44,6 @@ def extract_incoming_vol_data(spark: SparkSession, source, expected_schema: Stru
         df = incoming_vol_join_key_logic(df)
 
     return df
-
 
 def transform_column(col_name):
     """
@@ -88,9 +89,6 @@ def incoming_vol_join_key_logic(df: DataFrame) -> DataFrame:
     )
     return df
 
-
-
-
 def extract_activity_list_data(spark, nas_path: str) -> DataFrame:
     try:
         return spark.read.csv(nas_path, header=True, inferSchema=True)
@@ -107,51 +105,141 @@ def extract_emp_hierarchy_data(spark) -> DataFrame:
         logging.error(f"Failed to extract data from Hive table {hive_table}: {e}")
         raise
         
-def generate_step_2_output(primary_df_with_join_key: DataFrame, activity_list_df: DataFrame) -> DataFrame:
+def enrich_primary_with_activity_data(primary_df_with_join_key: DataFrame, activity_list_df: DataFrame) -> DataFrame:
+    """
+    Enriches the primary DataFrame with join key extracted with additional data from the activity list DataFrame.
+
+    Parameters:
+    - primary_df_with_join_key: The primary DataFrame with the join key extracted.
+    - activity_list_df: DataFrame containing activity list information.
+
+    Returns:
+    - A DataFrame resulting from the enriched primary data with activity-related insights.
+    """
     try:
-        step_2_output = primary_df_with_join_key.join(activity_list_df, primary_df_with_join_key["primary_activity"] == activity_list_df["activity_id"], "left") \
-            .select(
-                primary_df_with_join_key["*"],
-                coalesce(activity_list_df["New_Center"], lit("Not Defined")).alias("Center"),
-                coalesce(activity_list_df["Capacity_Planning_Group"], lit("Not Defined")).alias("Capacity_Planning_Group")
-            )
-        return step_2_output
+        enriched_output = primary_df_with_join_key.join(activity_list_df, primary_df_with_join_key["JoinKey"] == activity_list_df["JoinKey"], "left_outer").select(
+            primary_df_with_join_key["*"],
+            coalesce(activity_list_df["New_Center"], lit("Not Defined")).alias("Center"),
+            coalesce(activity_list_df["Capacity_Planning_Group"], lit("Not Defined")).alias("Capacity_Planning_Group")
+        )
+        return enriched_output
     except Exception as e:
-        logging.error(f"Error generating step_2_output: {e}")
+        logging.error(f"Error enriching primary data with activity data: {e}")
         raise
 
-def generate_step_3_output(step_2_output: DataFrame, emp_hierarchy_df: DataFrame) -> DataFrame:
+from pyspark.sql.functions import get_json_object
+def extract_employee_number_from_json(df: DataFrame, json_column_name: str, key_name: str) -> DataFrame:
+    """
+    Extracts a value from a JSON-encoded column and creates a new column with this value.
+
+    Parameters:
+    - df: DataFrame containing the JSON column.
+    - json_column_name: The name of the column containing the JSON string.
+    - key_name: The key within the JSON from which to extract the value.
+
+    Returns:
+    - DataFrame with a new column added for the extracted value.
+    """
+    json_path = f"$.{key_name}"
+    return df.withColumn("employee_number", get_json_object(col(json_column_name), json_path))
+
+
+def enrich_primary_with_emp_hierarchy(primary_df_with_join_key: DataFrame, emp_hierarchy_df: DataFrame, json_column_name: str) -> DataFrame:
+    """
+    Enriches the primary DataFrame with join key extracted with employee hierarchy information.
+
+    Parameters:
+    - primary_df_with_join_key: The primary DataFrame with the join key extracted.
+    - emp_hierarchy_df: DataFrame containing employee hierarchy information.
+    - json_column_name: The name of the column in primary_df containing the JSON string with employee information.
+
+    Returns:
+    - A DataFrame resulting from the enriched primary data with employee hierarchy insights.
+    """
     try:
-        step_3_output = step_2_output.join(emp_hierarchy_df, step_2_output["employee_number"] == emp_hierarchy_df["employeeid"], "left")
-        return step_3_output
+        # Extract 'employee_number' join key from the JSON column in primary_df
+        primary_df_with_employee_number = extract_employee_number_from_json(primary_df_with_join_key, json_column_name, "employee_number")
+        
+        # Perform the join operation using 'employee_number'
+        enriched_output = primary_df_with_employee_number.join(emp_hierarchy_df, primary_df_with_employee_number["employee_number"] == emp_hierarchy_df["employeeid"], "left_outer")
+        
+        return enriched_output
     except Exception as e:
-        logging.error(f"Error generating step_3_output: {e}")
+        logging.error(f"Error enriching primary data with employee hierarchy: {e}")
         raise
 
 
-def identify_join_columns(df1: DataFrame, df2: DataFrame) -> list:
+def find_common_join_keys(df1: DataFrame, df2: DataFrame) -> list:
+    """
+    Identifies common join keys between two DataFrames by comparing their column names.
+
+    Parameters:
+    - df1: First DataFrame.
+    - df2: Second DataFrame.
+
+    Returns:
+    - A list of common column names between df1 and df2.
+    """
     df1_columns = set(df1.columns)
     df2_columns = set(df2.columns)
-    return list(df1_columns.intersection(df2_columns))
+    common_columns = list(df1_columns.intersection(df2_columns))
+    return common_columns
 
-def join_step_2_and_3_data(step_2_output: DataFrame, step_3_output: DataFrame) -> DataFrame:
-    join_columns = identify_join_columns(step_2_output, step_3_output)
-    if not join_columns:
-        raise ValueError("No suitable join columns were identified between the two DataFrames.")
-    return step_2_output.join(step_3_output, join_columns, 'outer')
 
-def join_and_save_data(spark, primary_df, activity_list_df, emp_hierarchy_df, join_columns, intermediate_output_paths=None, final_output_path=None):
+def merge_enriched_data(enriched_activity_data: DataFrame, enriched_emp_hierarchy_data: DataFrame) -> DataFrame:
     """
-    Performs data join and save operations.
+    Merges two enriched DataFrames based on common join keys.
+
+    Parameters:
+    - enriched_activity_data: DataFrame enriched with activity data.
+    - enriched_emp_hierarchy_data: DataFrame enriched with employee hierarchy data.
+
+    Returns:
+    - A DataFrame resulting from merging the two input DataFrames.
     """
-    step_2_output = primary_df.join(activity_list_df, join_columns['primary_activity'], "left").select(primary_df["*"], coalesce(activity_list_df["New_Center"], lit("Not Defined")).alias("Center"), coalesce(activity_list_df["Capacity_Planning_Group"], lit("Not Defined")).alias("Capacity_Planning_Group"))
-    step_3_output = primary_df.join(emp_hierarchy_df, join_columns['primary_emp_hierarchy'])
+    common_keys = find_common_join_keys(enriched_activity_data, enriched_emp_hierarchy_data)
+    if not common_keys:
+        raise ValueError("No suitable join columns were identified between the two enriched DataFrames.")
+    
+    merged_data = enriched_activity_data.join(enriched_emp_hierarchy_data, common_keys, 'outer')
+    return merged_data
 
-    final_output = join_step_2_and_3_data(step_2_output, step_3_output)
 
-    if final_output_path:
-        final_output.write.mode("overwrite").parquet(final_output_path)
-    else:
-        return final_output
+def enhancement_workflow(spark, config):
+    """
+    Executes an ETL process based on the provided configuration, with a focus on flexibility and clarity.
+    """
+    try:
+        primary_df = extract_incoming_vol_data(spark, config['primary_data_source'])
+
+        enriched_activity_data = None
+        if config.get('include_activity_data_enrichment', False):
+            activity_list_df = extract_activity_list_data(spark, config['activity_list_data_source'])
+            enriched_activity_data = enrich_primary_with_activity_data(primary_df, activity_list_df)
+
+        enriched_emp_hierarchy_data = None
+        if config.get('include_employee_hierarchy_enrichment', False):
+            emp_hierarchy_df = extract_emp_hierarchy_data(spark, config['employee_hierarchy_data_source'])
+            enriched_emp_hierarchy_data = enrich_primary_with_emp_hierarchy(
+                primary_df, emp_hierarchy_df, config['employee_info_json_column'])
+
+        # Merging step 2 and step 3 outputs if both are performed
+        if enriched_activity_data is not None and enriched_emp_hierarchy_data is not None:
+            final_output = merge_enriched_data(enriched_activity_data, enriched_emp_hierarchy_data)
+        else:
+            final_output = enriched_emp_hierarchy_data or enriched_activity_data
+
+        if output_path := config.get('output_path'):
+            final_output.write.mode("overwrite").parquet(output_path)
+            print("Final output saved successfully.")
+        else:
+            return final_output
+
+    except Exception as e:
+        print(f"Failed to complete the enhancement_workflow due to: {e}")
+        raise
+
+
+
 
 
