03df08c223c1d6f3b6025dd1ad62f70202c25ec9
Abul Fahad <117757435+datacode09@users.noreply.github.com>
2024-04-11 15:37:05 -0400

    Update final_run.py

--- fucntional/final_run.py
+++ fucntional/final_run.py
@@ -4,8 +4,18 @@ from enhancement_module import get_spark_session, enhancement_workflow
 def main():
     """ Main function to run the ETL processes with minimal setup. """
     try:
-        # Configure logging
-        logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+        # Get the current date and time
+        current_time = datetime.now()
+        
+        # Format the current date and time as a string suitable for filenames
+        formatted_time = current_time.strftime('%Y-%m-%d_%H-%M-%S')
+        
+        # Create a dynamic log file name
+        log_filename = f'app_{formatted_time}.log'
+        
+        # Configure logging to write to a dynamically named file
+        logging.basicConfig(filename=log_filename, filemode='w', level=logging.DEBUG,
+                            format='%(asctime)s - %(levelname)s - %(message)s')
 
         # Load configuration
         with open('config.json', 'r') as config_file:
